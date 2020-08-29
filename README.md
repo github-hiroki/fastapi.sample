331ecc6f7cce8f17d4409c3f0103c89764f12001
hayashi <hayashi@source-pod.com>
2020-08-29 21:59:28 +0900

    add TwitterAPI proc by tweepy

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # README
 
-FastAPIなるものの存在を知ったのでサンプルを作ってみた。
+FastAPIなるものの存在を知ったのでTwitterAPIを使用して、Twitterの検索結果を取得するWebアプリケーションを作成してみました。
 
 ## 実行方法
 
@@ -13,3 +13,4 @@ FastAPIなるものの存在を知ったのでサンプルを作ってみた。
 ## 参考
 
 - [FastAPI公式](https://fastapi.tiangolo.com)
+- [tweepyドキュメント](https://docs.tweepy.org/en/latest/)
--- app/main.py
+++ app/main.py
@@ -1,20 +1,81 @@
 # Copyright 2020- hiroki.h Inc.
 
-from fastapi import FastAPI
+from fastapi import FastAPI, Request, Form
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+
+import datetime
+import dotenv
+import os
+import tweepy
 
 
 app = FastAPI(
-    title='FastAPIサンプル',
-    description='FastAPIサンプルの説明',
-    version='0.0.1',
+    title='FastAPIとTwitterAPIのサンプル',
+    description='FastAPIとTwitterAPIを使用したサンプルになります。',
+    version='0.0.2',
 )
+app.mount('/static', StaticFiles(directory='static'), name='static')
+
+templates = Jinja2Templates(directory='templates')
 
 
-@app.get("/")
-async def root():
-    return {'Hello': 'World'}
+class TweetContent:
+    """ツイートの内容を表現する。
+    """
+    def __init__(self, tweet):
+        self.username: str = tweet.user.name
+        self.userid: str = tweet.user.id
+        self.text: str = tweet.text
+        created_at = tweet.created_at + datetime.timedelta(hours=9)
+        self.created_at: str = created_at.strftime('%Y-%m-%d %H:%M:%S')
+        pass
+    pass
+
+
+def search_on_twitter(keyword: str):
+    """ハッシュタグで検索を行う。
+    """
+    # 環境変数ファイルを読み込む
+    dotenv.load_dotenv(os.path.join(os.path.dirname(__file__), '.env'))
+    # 認証情報
+    consumer_key = os.getenv('TWITTER_CONSUMER_KEY')
+    consumer_secret_key = os.getenv('TWITTER_CONSUMER_SECRET_KEY')
+    access_token = os.getenv('TWITTER_ACCESS_TOKEN')
+    access_token_secret = os.getenv('TWITTER_ACCESS_TOKEN_SECRET')
+
+    auth = tweepy.OAuthHandler(consumer_key, consumer_secret_key)
+    auth.set_access_token(access_token, access_token_secret)
+
+    # APIを取得
+    api = tweepy.API(auth)
+
+    # ツイートを検索(リツイートは除外する)
+    return [
+        TweetContent(tw)
+        for tw in tweepy.Cursor(api.search, q=keyword, result_type='recent').items(32)
+        if not(hasattr(tw, 'retweeted_status'))
+    ]
+
+
+@app.get("/", response_class=HTMLResponse)
+async def root(request: Request):
+    return templates.TemplateResponse(
+        'template.html',
+        {'request': request},
+    )
+
+
+@app.post("/", response_class=HTMLResponse)
+async def root(request: Request, keyword: str = Form(...)):
+    contents = search_on_twitter(keyword)
+    return templates.TemplateResponse(
+        'template.html',
+        {'request': request, 'keyword': keyword, 'contents': contents},
+    )
 
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host='0.0.0.0', port=8000)
